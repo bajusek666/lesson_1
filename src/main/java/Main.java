fcc7fc89c19fd94468fd5bd3b907fa38f3516d56
Kamil Bajus <kamil.bajus@onet.pl>
2022-11-25 17:01:01 +0100

    Zadanie 1 modified

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+
 public class Main {
     public static void main(String[] args) {
         Person slawek = new Person("Sławek","Bajus");
@@ -6,11 +8,21 @@ public class Main {
         Person sebastian = new Person("Sebastian", "Bajus");
         Person kamil = new Person("Kamil","Bajus");
 
-        System.out.println(slawek.toString());
-        System.out.println(dorota.toString());
-        System.out.println(angelika.toString());
-        System.out.println(sebastian.toString());
-        System.out.println(kamil.toString());
+        ArrayList<Person> people = new ArrayList<>();
+
+        people.add(slawek);
+        people.add(dorota);
+        people.add(angelika);
+        people.add(sebastian);
+        people.add(kamil);
+
+        for (Person person: people) {
+            System.out.println(person.getName() + " " + person.getSecondName());
+        }
+
+
+
+
 
     }
 }
